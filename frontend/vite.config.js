27dc78ae57af1fcc43b066ac69a379ad5bf00e7a
Tanay <tanaykamdi29@gmail.com>
2026-06-05 14:03:20 +0530

    adding the github actions

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config' // Import from vitest/config
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    // This adds support for the browser-like environment needed for React components
+    environment: 'jsdom',
+    // This allows you to use 'describe', 'it', and 'expect' without importing them in every file
+    globals: true,
+  },
 })
